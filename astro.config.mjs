580153b28190419afaf88e87b278157250d06173
Luke Davis <luke@lukealexdavis.co.uk>
2025-02-18 12:32:00 +0000

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import site from '/src/data/site.json';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: site.url,
+  integrations: [sitemap()]
+});
