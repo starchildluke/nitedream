d0bc11e2b3e567af6fd6fd1bf93d1e95d3d3f931
Luke Davis <luke@lukealexdavis.co.uk>
2025-09-19 00:28:11 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  server: { port: 4324 },
   site: site.url,
   integrations: [sitemap()]
 });
